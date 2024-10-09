1e8ef82525fe285ec0fb4c1e0ff5f1c5c739cfd6
Erik Strandberg <erikstrandberg153@gmail.com>
2024-10-09 15:43:05 +0200

    began working on boards page.

---
+++ src/boards.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en" class="m-0 p-0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home Page</title>
+    <link href="./output.css" rel="stylesheet" />
+    <link href="./papers.css" rel="stylesheet" />
+
+
+    <script
+      defer
+      src="https://kit.fontawesome.com/0a145d8537.js"
+      crossorigin="anonymous"
+    ></script>
+    <script defer src="dialog.js"></script>
+
+  </head>
+
+  <body
+    class="m-0 h-dvh w-full bg-zinc-900 p-0"
+  >
+    <?php include("include/navbar.php") ?>
+
+    <div class="flex flex-column">
+        <nav id="sidebar" class="bg-zinc-300 w-96 h-dvh">
+
+        </nav>
+        <div id="boardContainer" class="mt-14 text-white">
+            <h1>Here the boards will be, To, Do, Doing, Done</h1>
+        </div>
+    </div>
+  </body>
+</html>
